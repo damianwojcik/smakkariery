f6cf0d0047fd41c8f78a02fccc85e724dd1255f3
Damian Wójcik <khamian@gmail.com>
2016-07-19 10:35:14 +0200

    Added view of search.php based on archive.php

--- search.php
+++ search.php
@@ -1,19 +1,101 @@
 <?php get_header(); ?>
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-	<article class="simple-article">
+<!-- =================================================
+	section archive
+================================================== -->
+<section class="archive">
 
-		<a href="<?= get_permalink(); ?>">
+	<div class="container">
 
-			<h1 class="article-title"><?php the_title(); ?></h1>
+		<header>
 
-			<p><?= wp_trim_words($post->post_content, 50); ?></p>
-            
-		</a>
+			<h1>
+				Wyniki wyszukiwania dla: <?php echo get_search_query(); ?>
+			</h1>
 
-	</article>
+		</header>
 
-<?php endwhile; endif; ?>
+		<?php if ( have_posts() ) : ?>
+
+			<ul class="articles-list">
+
+				<?php while( have_posts() ) : the_post(); ?>
+
+				<?php
+
+					$thumbnail = wp_get_attachment_image_src ( get_post_thumbnail_id(), 'large');
+					$date = get_the_date();
+					$title = get_the_title();
+					$content = get_the_content();
+					$trimmed_content = wp_trim_words ( $content, 50 );
+
+				?>
+
+				<li>
+
+					<article class="news-entry">
+
+						<a href="<?php the_permalink(); ?>">
+
+							<div class="wrap" 
+								<?php if( !empty($thumbnail) ){ ?>
+									style="background-image: url('<?php echo $thumbnail[0]; ?>')"
+								<?php } ?>
+							>
+									<?php if( empty($thumbnail) ){ ?>
+										<div class="image-placeholder">
+							            	<i class="fa fa-picture-o" aria-hidden="true"></i>
+							            </div>
+									<?php } else { ?>
+										<div class="color-overlay"></div>
+									<?php } ?>
+								</div>
+							<!-- END wrap -->
+
+							<div class="text-wrap">
+
+								<div class="triangle-top" style="border-bottom-color: #E8E8E8"></div>
+								
+								<time><?php echo $date; ?></time>
+
+								<h2><?php echo $title; ?></h2>
+
+								<p>
+									<?php echo $trimmed_content; ?>
+								</p>
+
+								<div class="read-more">Czytaj więcej <i class="fa fa-chevron-right" aria-hidden="true"></i></div>
+
+							</div>
+							<!-- END text-wrap -->
+
+						</a>
+
+					</article>
+
+				</li>
+
+			<?php endwhile; ?>
+
+			</ul>
+
+			<div class="pagination">
+				<div class="nav-previous"><?php next_posts_link( 'Poprzednie' ); ?></div>
+				<div class="nav-next"><?php previous_posts_link( 'Następne' ); ?></div>
+			</div>
+			<!-- END pagination -->
+
+		<?php else : ?>
+
+			<p><?php _e('Przepraszamy, niestety nie znaleziono żadnych wpisów spełniających Twoje kryteria.'); ?></p>
+
+		<?php endif; ?>
+
+	</div>
+
+</section>
+<!-- END section archive -->
+
 
 <?php get_footer(); ?>
